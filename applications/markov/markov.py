0896d45de8dd0e61da043e93d8dcfa9c3b0ac847
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-05-07 15:47:13 -0700

    finished markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,9 +1,41 @@
 import random
 
 # Read in all the words in one go
-with open("input.txt") as f:
-    words = f.read()
-
 # TODO: analyze which words can follow other words
+glossary = {}
 
+def build_glossary():
+	with open("input.txt") as f:
+		all_words = f.read()
+
+		words = all_words.split(" ")
+
+		for index in range(0, len(words) - 1):
+			current = words[index]
+			next = words[index + 1]
+
+			if current in glossary:
+				glossary[current].append(next)
+			else:
+				glossary[current] = []
+				glossary[current].append(next)
+			
+	return glossary
+
+build_glossary()
 # TODO: construct 5 random sentences
+
+start = [word for word in glossary.keys() if word.istitle()]
+
+for i in range(5):
+	first_word = random.choice(start)
+	word = first_word
+	result = word
+	while word:
+		if word[-1] in ['.', '?', '!']:
+			break
+		else:
+			word = random.choice(glossary[word])
+			result += " " + word
+	print(result)
+	print('\n')
